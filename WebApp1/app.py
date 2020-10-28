004e91d71c0ed889ac3eee015837bf4afec47450
su795 <64509903+su795@users.noreply.github.com>
2020-10-28 09:37:37 +0900

    2020-10-28 Python 백업 예제

---
+++ WebApp1/app.py
@@ -0,0 +1,21 @@
+"""
+    날짜 : 2020/06/25
+    이름 : 오세훈
+    내용 : 파이썬 웹 프로그래밍
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello Python Flask!!!</h1>'
+
+@app.route('/hello')
+def hello():
+    return render_template('/hello.html')
+
+
+
+if __name__=='__main__':
+    app.run()
