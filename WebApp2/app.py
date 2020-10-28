f5ecf40c16f283591720a6ec2ed66bcdf9b3fe91
su795 <64509903+su795@users.noreply.github.com>
2020-10-28 09:37:50 +0900

    2020-10-28 Python 백업 예제

---
+++ WebApp2/app.py
@@ -0,0 +1,30 @@
+"""
+    날짜 : 2020/06/25
+    이름 : 오세훈
+    내용 : 파이썬 웹 프로그래밍 조건문, 반복문
+"""
+from flask import Flask, render_template
+# File > Setting > Project: Python > Project Interpreter > Flask 설치
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    
+    title = '파이썬 Flask 제어문 실습'
+    content = ''
+    dataset = {
+        'title': '대한민국 도시',
+        'cities': ['서울', '대전', '대구', '부산', '광주']
+    }
+        
+    return render_template('/index.html', tit=title, cont=content, data=dataset)
+
+
+@app.route('/main')
+def main():
+    return render_template('/main.html')
+        
+
+if __name__ == '__main__':
+    app.run()
